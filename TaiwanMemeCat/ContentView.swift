c10c3b4334775ff178fa2c2473934e9227aab635
Ricky Chuang <songsongyyohya@gmail.com>
2021-10-15 22:58:07 +0800

    Initial Commit

---
+++ TaiwanMemeCat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaiwanMemeCat
+//
+//  Created by 莊智凱 on 2021/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaiwanMemeCat/TaiwanMemeCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaiwanMemeCatApp.swift
+//  TaiwanMemeCat
+//
+//  Created by 莊智凱 on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct TaiwanMemeCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
